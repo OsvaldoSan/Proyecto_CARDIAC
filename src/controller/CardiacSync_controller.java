d2c9de885656c9c67447a01d612097f5d3bf02c9
osvaldo_san <osvaldosantos823@gmail.com>
2023-11-25 18:12:37 -0600

    Se añadio un nuevo TitlePane para la memoria secundaria
    En editLayout al final se encuentran los nuevos cambios, falta que la altura del nuevo este estable, así como el ancho. También decidir
    como se mostraran las instrucciones en la memoria secundaria

--- src/controller/CardiacSync_controller.java
+++ src/controller/CardiacSync_controller.java
@@ -1,20 +1,29 @@
 package controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.RowConstraints;
+import javafx.scene.text.Text;
 
 public class CardiacSync_controller extends Cardiac{
     // Design Variables
     @FXML
     private GridPane gridPMachineStatus;
 
+    @FXML
+    private HBox HBoxBottom;
+
     Label gDescriptionStarter = new Label("Starter"),gDescriptionSwitcher=new Label("Switcher");
     Label gStarter= new Label("Working"),gSwitcher=new Label("SO");
 
     @FXML
     public void editLayout(){
+
+        // -------------------------------------------- Section Left Bar ------------------------------------------------------------------
+
         // Grid pane for Machine Status
         // CSS definitions for labels
         gStarter.setId("labelNumeric");
@@ -32,9 +41,28 @@ public class CardiacSync_controller extends Cardiac{
         gridPMachineStatus.getRowConstraints().add(newRowConstraints);
         //Add switcher
         gridPMachineStatus.addRow(newRow+1,gDescriptionSwitcher,gSwitcher);
-
-
         gridPMachineStatus.getRowConstraints().add(newRowConstraints);
+
+        // ----------------------------------------- Section bottom right -------------------------------------------------------
+
+        TitledPane newTitlePane = new TitledPane();
+        HBox.setHgrow(newTitlePane, Priority.ALWAYS);
+
+
+
+
+        newTitlePane.setText("Memoria Secundaria");
+        // Add a TextArea to the newTitledPane
+        TextArea textArea = new TextArea();
+        textArea.setPromptText("Empty");
+        textArea.setTextFormatter(new TextFormatter<String>(change ->
+                change.getControlNewText().length() <= 100 ? change : null));
+
+        newTitlePane.setCollapsible(false); // Optional: Make the TitledPane non-collapsible
+        newTitlePane.setContent(textArea);
+
+        HBoxBottom.getChildren().add(newTitlePane);
+
 
 
 
